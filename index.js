1df406fdc7c6331997b64c3176087fbbeb2aceda
Melissa Rudick <mel.madrugada@gmail.com>
2017-08-19 05:04:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ function sayHiToGrandma(string) {
 function sayHiToGrandma(string) {
   if (string === string.toUpperCase()) {
     return "YES INDEED!"
+  } else {
+    return "I love you, too."
   }
 }
 function logWhisper(string) {
